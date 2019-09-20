fd0138094c382beec8b17a5deb046d1ab1525b83
LineEgerod <line.egerod@gmail.com>
2019-09-20 10:51:39 +0200

    Added VEP-step

--- code/pipeline/sentieon_RNAseq_frozen.sh
+++ code/pipeline/sentieon_RNAseq_frozen.sh
@@ -190,8 +190,12 @@ mv output-TNScope.vcf.gz "$sample"-TNScope.vcf.gz
 mv output-hc.vcf.gz.tbi "$sample"-hc.vcf.gz.tbi
 mv output-TNScope.vcf.gz.tbi "$sample"-TNScope.vcf.gz.tbi 
 
-
 $apps/computerome/submit.py "$apps/ngs-tools/vcf_statistics.sh "$sample"-hc.vcf.gz hc" --name "$sample"-hc-vcf_statistics -np 1 --no-numbering
+
+# ******************************************
+# 8. Generating VEP-files
+# ******************************************
+$apps/computerome/submit.py "$apps/ngs-tools/vep.sh "$sample"-hc" --name "$sample"-hc-vcf_vep -np=1 --no-numbering --hours=2 -mem=50 
 
 # remove all the files we don't want to keep:
 rm recal* 

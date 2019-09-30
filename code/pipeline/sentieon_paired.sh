f7b32ff26fc8bacb5e77aeafd46341df2a88e5bd
Nanna Møller Barnkob <s143372@student.dtu.dk>
2019-09-30 15:41:40 +0200

    Pipeline for finding somatic variants. Also runs vcf-statistics. Should be submitted with somatic_setup.py

--- code/pipeline/sentieon_paired.sh
+++ code/pipeline/sentieon_paired.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-
-
+# *******************************************
+# SENTIEON TUMOR/NORMAL VERSION 1.0 for paired analysis
+# Dated 29-09-2019
+# *******************************************
+pipeline_version="PSP01"
 
 # *******************************************
 # ASSUMPTIONS
-# two bam files are received from python submit scripts
-# both bam files are produced by sentieon RNA / WGS pipeline and already recalibrated
+# two bam files are received from python submit scripts (somatic_setup.py)
+# both bam files are produced by sentieon RNA / WGS pipeline and are already recalibrated
 # *******************************************
 
 # *******************************************
 #                  SET-UP
 # *******************************************
 
-# SENTIEON TUMOR/NORMAL VERSION 1.0 for paired analysis
-# Dated 29-09-2019
-pipeline_version="PSP01"
-
 apps="/home/projects/HT2_leukngs/apps/github/code"
 
 tumor_path=$1
-germline_path=$2
+normal_path=$2
 destination=$3
 
 # Number of threads
@@ -34,18 +33,19 @@ fi
 
 tumor_name=$(echo $(basename $tumor_path) | sed 's/.bam//g')
 normal_name=$(echo $(basename $normal_path) | sed 's/.bam//g')
-output_name="$tumor_name"$normal_name"$pipeline_version"
+output_name=$(echo $(basename $destination))
 
 
-echo "# Input files: $tumor_path and $germline_path"
-echo "# Names: $tumor_name, $normal_name, $output_name"
-echo "# Results are stored in $destination"
+echo -e "# Name \t Filepath: "
+echo -e "# $tumor_name" \t "$tumor_path "
+echo -e "# $normal_name" \t "$normal_path "
+echo -e "# $output_name" \t "$destination " 
 echo "# Number of threads:" $nt
 echo "# Reference version: b37"
 
 set -x
-#data_dir is  the directory of the input symlinks (symlinks should not be followed,which is configured with -s option)
-data_dir="$( dirname "$(realpath -s $1)" )"
+
+mkdir $destination 
 
 # Update with the location of the Sentieon software package
 module load cbspythontools/1.0
@@ -54,7 +54,7 @@ export SENTIEON_LICENSE=localhost:8990
 
 # Resources
 reference_dir=/home/databases/gatk-legacy-bundles/b37
-fasta=$reference_dir/human_g1k_v37_decoy.fasta								#ucsc.hg19_chr22.fasta
+fasta=$reference_dir/human_g1k_v37_decoy.fasta
 dbsnp=$reference_dir/dbsnp_138.b37.vcf
 
 # *******************************************
@@ -64,13 +64,14 @@ dbsnp=$reference_dir/dbsnp_138.b37.vcf
 ## 1. Variant calling
 $SENTIEON_INSTALL_DIR/bin/sentieon driver -t $nt -r $fasta \
   -i $tumor_path \
-  -i $germline_path \
+  -i $normal_path \
   --algo TNscope \
   --tumor_sample $tumor_name --normal_sample $normal_name \
   --dbsnp $dbsnp \
   $destination/$output_name-TNscope.vcf.gz
 
 ## 2. Statistics
-$apps/ngs-tools/vcf_statistics.sh $destination/$output_name-TNscope.vcf.gz somatic
+cd $destination 
+$apps/ngs-tools/vcf_statistics.sh $output_name-TNscope.vcf.gz somatic
 
 ## 3. Analysis ...

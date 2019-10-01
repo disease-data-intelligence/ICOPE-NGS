3128900f581afc4b284f9b4268faf4289252e05f
Nanna Møller Barnkob <s143372@student.dtu.dk>
2019-10-01 14:29:33 +0200

    No interval option

--- code/pipeline/all_pipelines/sentieon_paired_PSP01.sh
+++ code/pipeline/all_pipelines/sentieon_paired_PSP01.sh
@@ -69,7 +69,6 @@ $SENTIEON_INSTALL_DIR/bin/sentieon driver -t $nt -r $fasta \
   --algo TNscope \
   --tumor_sample $tumor_name --normal_sample $normal_name \
   --dbsnp $dbsnp \
-  --interval 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y \
   $destination/$output_name-TNscope.vcf.gz
 
 ## 2. Statistics on all somatic variants

23e4e04d90fe796cc4313a3bb01b7cc0b08ff882
nannabarnkob <nannambarnkob@gmail.com>
2019-11-18 09:25:34 +0100

    wrong destination

--- code/computerome/somatic_setup.py
+++ code/computerome/somatic_setup.py
@@ -52,11 +52,12 @@ def find_pairs(samples, germline_pipeline, tumor_pipeline, paired_pipeline):
             if r.endswith('.'+tumor_pipeline):
                 tumor_name = r.split('/')[-1]
                 tumor = r + '/' + [x for x in f if (x.endswith('.bam') and x.startswith(tumor_name))][0]
+                tumor_path = '/'.join(r.split('/')[:-2])
                 print("# Found tumor file", tumor)
         if sum(map(lambda x: isinstance(x, str), (tumor, germline))) == 2:
             mrd = tumor_name.split('_')[0]
             dest_filename = mrd + '-' + tumor_name.split('_')[1] + '-' + germline_name.split('_')[1] + '-' + paired_pipeline
-            dest = '/'.join(r.split('/')[:-1]) + '/' + dest_filename
+            dest =  tumor_path + '/' + dest_filename
             print("# Defining destination for somatic variants:", dest)
             yield (tumor, germline, dest)
         else:

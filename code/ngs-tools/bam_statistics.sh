35c59bcf5ad8b3f9254b3c6f028e7a7517e59865
nannabarnkob <nannambarnkob@gmail.com>
2019-11-19 15:27:00 +0100

    updates

--- code/ngs-tools/bam_statistics.sh
+++ code/ngs-tools/bam_statistics.sh
@@ -6,25 +6,27 @@ module load bedtools/2.28.0
 
 echo "Using computerome modules samtools/1.9 and bedtools/2.28.0 and python3 modules listed below" 
 bam=$1
+sample=$(basename $bam | sed 's/.bam.*//')
 
 start=`date +%s`
 
 apps="/home/projects/HT2_leukngs/apps/github/code"
-# make directory if not created already 
-mkdir -p quality_reports
+# make directory if not created already
+destination="$sample".quality_reports
+mkdir -p $destination
 
 echo "# Getting coverage statistics"
 bed=/home/projects/HT2_leukngs/data/references/hg37/USCS.hg37.canonical.exons.filtered.bed
 genepanel=/home/projects/HT2_leukngs/data/references/general/genepanel.txt
 
-samtools bedcov $bed $bam > quality_reports/cov.canonical.exons.bed
+samtools bedcov $bed $bam >  $destination/cov.canonical.exons.bed
 echo "# Summarizing exon and gene coverage"
-$apps/quality/exon_coverage.py quality_reports/cov.canonical.exons.bed $genepanel 
-$apps/quality/gene_coverage.py quality_reports/cov.canonical.exons.bed $genepanel
+$apps/quality/exon_coverage.py  $destination/cov.canonical.exons.bed $genepanel
+$apps/quality/gene_coverage.py  $destination/cov.canonical.exons.bed $genepanel
 
 echo "# Getting coverage pr. chromosome"
-bedtools genomecov -ibam $bam -max 150 > quality_reports/genome.cov
-$apps/quality/chr_coverage.py quality_reports/genome.cov 150
+bedtools genomecov -ibam $bam -max 150 >  $destination/genome.cov
+$apps/quality/chr_coverage.py  $destination/genome.cov 150
 
 end=`date +%s`
 runtime=$((end-start))
